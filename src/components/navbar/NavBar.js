4702b99dc412c5ed1c0ad170d1740f4cd28f771a
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-03-14 19:52:24 -0300

    navbar 1

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,32 +1,35 @@
-
+import logo from '../../assets/images/logo.png'
 import './NavBar.css'
-//import 'src/components/navbar/NavBar.css';
+
 function NavBar() {
-    return(
+    return (
         <div className="nav-bar">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                <a className="navbar-brand" href="#">Conexões do Bem</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav me-auto">
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Início</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Buscar</a>
-                    </li>
-                </ul>
-                <button className="btn btn-primary me-2">SOU UMA ONG</button>
-                <button className="btn btn-danger">QUERO DOAR</button>
+                    <div className='area-logo'>
+                        <a className="navbar-brand" href="#"><img src={logo} className="logo" /></a>
+                    </div>
+                    
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav me-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Início</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Buscar</a>
+                            </li>
+                        </ul>
+                        <button className="btn btn-primary me-2">SOU UMA ONG</button>
+                        <button className="btn btn-danger">QUERO DOAR</button>
+                    </div>
                 </div>
-            </div>
             </nav>
         </div>
     );
-    
+
 }
 
 export default NavBar;
